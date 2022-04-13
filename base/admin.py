dda9ace86e030366900dff7616e21c0d8040dad4
PolymetisOutis <kgwtmhk@gmail.com>
2022-04-13 11:45:43 +0900

    Flag admin added

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import CustomUser, Champion, Category, Enemy
+from .models import CustomUser, Champion, Category, Enemy, Flag
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Champion)
 admin.site.register(CustomUser)
 admin.site.register(Enemy)
+admin.site.register(Flag)
