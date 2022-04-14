58b54241c2dba14c6a87ad5c134e18f986368913
PolymetisOutis <kgwtmhk@gmail.com>
2022-04-14 15:17:19 +0900

    admin list_display

--- base/admin.py
+++ base/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import CustomUser, Champion, Category, Enemy, Flag
 # Register your models here.
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Champion)
 admin.site.register(CustomUser)
 admin.site.register(Enemy)
